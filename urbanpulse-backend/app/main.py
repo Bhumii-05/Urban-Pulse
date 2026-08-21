88bbc5679041cee541abf04bcf2ee5f4254f3073
Sneha Kesharwani <snehakesharwani05@gmail.com>
2026-08-21 23:16:16 +0530

    Revert "feat(auth): integrate signup, login, and role-based redirect with axios client"

--- urbanpulse-backend/app/main.py
+++ urbanpulse-backend/app/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.v1.auth import router as auth_router
 from app.api.v1.user import router as users_router
@@ -10,21 +9,6 @@ from app.api.v1 import concerns
 app = FastAPI(
     title="UrbanPulse API",
     version="1.0.0",
-)
-
-#CORS config
-
-origins = [
-    "http://localhost:5173",  # Default Vite dev server URL
-    "http://127.0.0.1:5173",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,            
-    allow_credentials=True,           
-    allow_methods=["*"],             
-    allow_headers=["*"],            
 )
 
 
