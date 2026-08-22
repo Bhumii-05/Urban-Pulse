9e783fc1a03450807e4558d450845322ba5cab88
Sneha Kesharwani <snehakesharwani05@gmail.com>
2026-08-23 03:53:40 +0530

    Update admin dashboard

--- urbanpulse-frontend/src/App.jsx
+++ urbanpulse-frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import SignUp from './pages/SignUp'
 import CitizenDashboard from './pages/CitizenDashboard'
+import AdminDashboard from './pages/AdminDashboard'
 
 export default function App() {
   return (
@@ -11,6 +12,8 @@ export default function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/citizen" element={<CitizenDashboard />} />
+      <Route path="/admin" element={<AdminDashboard />} />
+    
     </Routes>
   )
 }
