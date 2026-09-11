db39670708dee4b6c4870d1c41208a194001e556
himu0023 <himanshubisht3231@gmail.com>
2026-09-11 19:10:12 +0530

    updated main with project url

--- urbanpulse-chatbot/app/main.py
+++ urbanpulse-chatbot/app/main.py
@@ -20,7 +20,7 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:5174",
         "http://127.0.0.1:5174",
-        "https://urban-pulse-lake.vercel.app/"
+        "https://urban-pulse-lake.vercel.app",
     ],
     allow_origin_regex=r"http://(localhost|127\.0\.0\.1)(:\d+)?",
     allow_credentials=True,
